648b44c416d9724d32de7b1f488e24cbbdf0314f
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2025-12-14 15:24:06 -0300

    Inicio de migración a SQLite

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,27 +1,97 @@
+import sqlite3
 
+class TaskErrorManager(Exception):
+    pass
+
+class TaskNotFoundError(TaskErrorManager):
+    """Error. Tarea no existe."""
+    def __init__(self, task_id):
+        self.task_id = task_id
+        super().__init__(f"Error. Tarea con ID '{task_id}' no encontrada.")
+    
 class Task:
     def __init__(self, id, description):
         self.id = id
         self.description = description
         self.completed = False
     
+    def is_completed(self):
+        return self.completed
+    
 class TaskManager:
-    def __init__(self):
-        self.pending_tasks = {}
-        self.next_id = 0
+    def __init__(self, db_name):
+        #self.tasks = {}
+        #self.next_id = 0
+        #Creamos las base de datos
+        self.conn = sqlite3.connect(db_name)
+    
+        #Configuramos el cursor
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+
+        #Inicializamos la tabla
+        self._create_table()
+
+    def _create_table(self):   
+        "Creamos la tabla si no existe"
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS tareas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                descripcion TEXT NOT NULL,
+                completada BOOLEAN NOT NULL DEFAULT 0
+            )
+        """)
+        self.conn.commit()
+    
+    def close(self):
+        self.conn.close()
 
     def add_task(self, description):
-        #Creamos el nuevo objeto tarea
-        new_task = Task(
-            id=self.next_id,
-            description=description
-        )
-        self.pending_tasks[self.next_id] = new_task
-        self.next_id += 1
-        return self.next_id - 1
-        
+
+        sql = "INSERT INTO tareas (descripcion, completada) VALUES (?, ?)"
+        try:
+            self.cursor.execute(sql, (description, 0))
+        except sqlite3.Error as e:
+            #Handleamos
+            print(f"Error al insertar la tarea {e}")
+            raise
+        #Guardamos los cambios
+        self.conn.commit()
+        #Handleamos el id
+        generated_id = self.cursor.lastrowid
+
+        return generated_id
+    
+    def get_pending_tasks(self):
+        pending_tasks = []
+        for task in self.tasks.values():
+            if not task.is_completed():
+                pending_tasks.append(task)
+        return pending_tasks
+
+    def complete_task(self, task_id):
+        self.assert_is_valid_task_id(task_id)
+        task = self.tasks.get(task_id)
+        task.completed = True
+
     def contains_task(self, task_id):
-        return task_id in self.pending_tasks
+        return task_id in self.tasks
+    
+    def is_completed(self, task_id):
+        self.assert_is_valid_task_id(task_id)
+        task = self.tasks.get(task_id)
+        return task.is_completed()
     
     def tasks_count(self):
-        return len(self.pending_tasks)
+        #Contamos las tareas totales
+        sql = "SELECT COUNT(*) FROM tareas"
+        #Ejecutamos
+        self.cursor.execute(sql)
+        
+        count = self.cursor.fetchone()[0]
+        return count
+    
+    #Assert methods
+    def assert_is_valid_task_id(self, task_id):
+        if(task_id not in self.tasks):
+            raise TaskNotFoundError(task_id)
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,22 +1,75 @@
 import unittest
-from src.task_manager import TaskManager
+from src.task_manager import TaskManager, TaskNotFoundError
 
 class TestTaskManager(unittest.TestCase):
     def setUp(self):
-        self.generic_task_description = 'Leer capítulo 5 del libro Design Patterns'
+        #Creamos el task manager
+        self.manager = TaskManager(db_name='test_tasks.db')
+        #Tareas genéricas
+        self.generic_task_description_one = 'Leer capítulo 5 del libro Design Patterns'
+        self.generic_task_description_two = 'Hacer ejercicio 5 minutos'
+        self.generic_task_description_three = 'Correr 1 hora'
+        #Limpiamos
+        self.manager.cursor.execute("DELETE FROM tareas")
+        self.manager.conn.commit()
+    
+    def tearDown(self):
+        self.manager.close()
+
     def test_can_add_task(self):
-        manager = TaskManager()
+        
         #Agregamos la tarea
-        created_task_id = manager.add_task(self.generic_task_description)
+        created_task_id = self.manager.add_task(self.generic_task_description_one)
         #Asserts
-        self.assertEqual(manager.tasks_count(), 1, "Debe haber una tarea")
-        self.assertTrue(manager.contains_task(created_task_id), "Debe estar exactamente esta tarea")
+        self.assertEqual(self.manager.tasks_count(), 1, "Debe haber una tarea")
+        self.assertTrue(self.manager.contains_task(created_task_id), "Debe estar exactamente esta tarea")
+
+    def test_can_list_only_pending_tasks(self):
+        
+        #Agregamos 3 tareas
+        task_id_one = self.manager.add_task(self.generic_task_description_one)
+        task_id_two = self.manager.add_task(self.generic_task_description_two)
+        task_id_three = self.manager.add_task(self.generic_task_description_three)
+        #Marcamos la segunda tarea como completada para ver si es excluida
+        self.manager.complete_task(task_id_two)
+        #Conseguimos las listas de las tareas pendientes
+        pending_tasks = self.manager.get_pending_tasks()
+        #Vemos que tenga longitud 2
+        self.assertEqual(len(pending_tasks), 2, "Debe haber exactamente 2 tareas pendientes")
+        #Vemos que estén exactamente las tareas correctas
+        for task in pending_tasks:
+            self.assertFalse(task.is_completed(), "Todas las tareas están sin completar")
+        
 
     def test_added_task_is_not_completed(self):
-        manager = TaskManager()
+        
         #Agregamos la tarea
-        created_task_id = manager.add_task(self.generic_task_description)
-        self.assertFalse(manager.is_completed(created_task_id))
+        created_task_id = self.manager.add_task(self.generic_task_description_one)
+        #Asserts
+        self.assertFalse(self.manager.is_completed(created_task_id))
+
+    def test_can_mark_task_as_completed(self):
+        
+        #Agregamos la tarea
+        created_task_id = self.manager.add_task(self.generic_task_description_one)
+        self.manager.complete_task(created_task_id)
+        #Asserts
+        self.assertTrue(self.manager.is_completed(created_task_id))
+
+    def test_mark_invalid_task_id_as_completed_raises_error(self):
+        
+        non_existent_id = 10
+        #Asserts
+        with self.assertRaises(TaskNotFoundError):
+            self.manager.complete_task(non_existent_id)
+
+    def test_check_is_completed_invalid_task_id_raises_error(self):
+        
+        non_existent_id = 10
+        #Asserts
+        with self.assertRaises(TaskNotFoundError):
+            self.manager.is_completed(non_existent_id)
+
 if __name__ == '__main__':
     unittest.main()
 
